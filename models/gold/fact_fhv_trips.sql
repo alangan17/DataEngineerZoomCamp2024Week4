b76b8af93f4fa39d0b61e02942a2c13898251ae7
Alan Ngan <alangan17@outlook.com>
2024-02-19 19:12:14 +0000

    Add fhv models

---
+++ models/gold/fact_fhv_trips.sql
@@ -0,0 +1,52 @@
+{{ config(materializaed="table") }}
+
+with
+    fhv_data as (
+        select {{ dbt_utils.star(from=ref("stg_fhv_tripdata")) }}
+        from {{ ref("stg_fhv_tripdata") }}
+    ),
+    trips_unioned as (
+        select *
+        from fhv_data
+    ),
+    dim_zones as (
+        select {{ dbt_utils.star(from=ref("dim_zones")) }} 
+        from {{ ref("dim_zones") }}
+        where borough != 'Unknown'
+    ),
+    final as (
+        select
+            trips_unioned.`vendor_id`,
+            trips_unioned.`ratecode_id`,
+            trips_unioned.`pickup_location_id`,
+            pickup_zone.borough as pickup_borough,
+            pickup_zone.zone as pickup_zone,
+            trips_unioned.`dropoff_location_id`,
+            dropoff_zone.borough as dropoff_borough,
+            dropoff_zone.zone as dropoff_zone,
+            trips_unioned.`pickup_datetime`,
+            trips_unioned.`dropoff_datetime`,
+            trips_unioned.`store_and_fwd_flag`,
+            trips_unioned.`passenger_count`,
+            trips_unioned.`trip_distance`,
+            trips_unioned.`fare_amount`,
+            trips_unioned.`extra`,
+            trips_unioned.`mta_tax`,
+            trips_unioned.`tip_amount`,
+            trips_unioned.`tolls_amount`,
+            trips_unioned.`improvement_surcharge`,
+            trips_unioned.`total_amount`,
+            trips_unioned.`payment_type`,
+            trips_unioned.`congestion_surcharge`,
+            trips_unioned.`payment_type_description`
+
+        from trips_unioned
+        inner join
+            dim_zones as pickup_zone
+            on trips_unioned.pickup_location_id = pickup_zone.locationid
+        inner join
+            dim_zones as dropoff_zone
+            on trips_unioned.dropoff_location_id = dropoff_zone.locationid
+    )
+select *
+from final
